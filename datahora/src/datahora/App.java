41bcf08038727e08c338499e25a2e61a38aca2cd
Marcelina Lima <marcelina.batista@hotmail.com>
2023-03-30 11:26:33 -0300

    Adicionando  modificações no arquivos datahora

--- datahora/src/datahora/App.java
+++ datahora/src/datahora/App.java
@@ -1,18 +1,20 @@
 package datahora;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class App {
-	static final long MILIS_POR_DIA =  100*60*60*24;
+	
+	static final long MILIS_POR_DIA =  100 * 60 * 60 * 24;
 	
 	public static void main(String[] args) {
 		
-		String formato = "dd/"
-		DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
+		String formato = "dd/MM/yy hh:mm:ss:SSS";
+		DateFormat df =  new SimpleDateFormat(formato);
 		
 		Date date = new Date();
-		long tempo = date.getTime();
+		/*long tempo = date.getTime();*/
 		
 		/*
 		 * System.out.println("Milis:" + tempo );
@@ -33,10 +35,16 @@ public class App {
 		*sb.append("\nAnos:");
 		*sb.append(tempo / MILIS_POR_DIA /365);
 		
+	
 		*System.out.println(sub.toString());
 		*/
 		
 		StringBuilder sb =new StringBuilder();
+		sb.append("Data Formatada:");
+		sb.append(df.format(date));
+		
+		System.out.println(sb.toString());
+		
 	}
 
 }
--- datahora/src/datahora/Calendario.java
+++ datahora/src/datahora/Calendario.java
@@ -5,14 +5,16 @@ import java.util.Date;
 
 public class Calendario {
 
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		
 		Calendar cal = Calendar.getInstance();	
 		Date hoje = new Date();
 		
 		cal.setTime(hoje);
+		
 		System.out.println(cal.get(cal.YEAR));
-		System.out.println(cal.get(cal.MONTH));
+		System.out.println(cal.get(cal.MONTH) + 1);
 		System.out.println(cal.get(cal.DAY_OF_MONTH));
 		
 		int hora = cal.get(cal.HOUR_OF_DAY);

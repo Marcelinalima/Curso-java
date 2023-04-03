9a953f388bb86da1f2648b43090683671c6cc1bb
Marcelina Lima <marcelina.batista@hotmail.com>
2023-04-03 16:57:48 -0300

    Adicionando modificaçoes Exercicios poo

--- aulas_classesabstract_exer/src/ex6/ContaEspecial.java
+++ aulas_classesabstract_exer/src/ex6/ContaEspecial.java
@@ -1,14 +1,13 @@
 package ex6;
 
-public class ContaEspecial  extends ContaCorrente{
+public class ContaEspecial extends ContaBancaria {
+	
 	
 	private double limite;
-	
-	public ContaEspecial() {}
 
-	public ContaEspecial(String nome, double saldo, double limite) {
-		super(nome, saldo);
-		this.limite =limite;
+	public ContaEspecial(double limite) {
+		super();
+		this.limite = limite;
 	}
 
 	public double getLimite() {
@@ -21,9 +20,17 @@ public class ContaEspecial  extends ContaCorrente{
 
 	@Override
 	public String toString() {
+		
 		return "ContaEspecial [limite=" + limite + "]";
 	}
-
-	
+	public boolean sacar (double valor) {
+		double saldoComLimite =  this.getSaldo()+ limite;
+		
+		if((saldoComLimite-valor) >=0) {
+		this.setSaldo (this.getSaldo()- valor);
+		return  true;
+		}
+		return false;
+	}
 
 }
